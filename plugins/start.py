0818c3e0cc8ac8b33d50fb97f367c4936ebd9ff6
Pʀᴇᴅᴀᴛᴏʀ <77227205+PredatorHackerzZ@users.noreply.github.com>
2022-02-19 13:52:56 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -90,8 +90,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("📽 Channel ", url = "https://t.me/TellyFun_Official")
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
+                    InlineKeyboardButton("📽 Channel ", url = "https://t.me/TellyFun_Official"),
+                    InlineKeyboardButton("😊 About Me", callback_data = "about")
                 ],
                 [
                     InlineKeyboardButton("🔒 Close", callback_data = "close")
